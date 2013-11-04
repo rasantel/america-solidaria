73bbdfa34012fc602578e67b820205fa03f1c34e
Sean T Fitzgerald <fitzgerald.sean.t@gmail.com>
2013-11-04 14:25:37 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmericaSolidaria::Application.config.secret_key_base = '4a5bcd4c4c932630ee1489d4fb77e15df66cea702c78f1b4c1aa8f13d9706c404d8352b0f5ecc458481dbbdc6d11e5bf098c657cdadab8a9f90f3f264a72146e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmericaSolidaria::Application.config.session_store :cookie_store, key: '_america-solidaria_session'
